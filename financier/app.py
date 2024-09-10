3678c0623329b516bb080fbc7caa638bb57af154
Joe Foster <joe.foster@protonmail.com>
2024-09-10 13:10:28 -0700

    title and axis labels

--- financier/app.py
+++ financier/app.py
@@ -3,8 +3,7 @@ import pandas as pd
 
 from financier import PROJECT_ROOT
 
-plt.rcParams["figure.figsize"] = [7.00, 3.50]
-plt.rcParams["figure.autolayout"] = True
+
 columns = ["Date", "Balance"]
 df = pd.read_csv(
     PROJECT_ROOT / "data/account_data.csv", usecols=columns, parse_dates=["Date"]
@@ -12,5 +11,13 @@ df = pd.read_csv(
 
 df["Balance"] = df["Balance"].replace({"£": "", r"\+": ""}, regex=True).astype(float)
 
+
+plt.rcParams["figure.figsize"] = [7.00, 3.50]
+plt.rcParams["figure.autolayout"] = True
+
 plt.plot(df.Date, df.Balance)
+
+plt.title("Account Balance Over Time")
+plt.xlabel("Date")
+plt.ylabel("Balance (£)")
 plt.savefig(PROJECT_ROOT / "output/balance_plot.png")
